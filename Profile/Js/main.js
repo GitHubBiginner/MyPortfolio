b86855fc3f19f29156a6d1ccb90e97194a56e7a3
Naoki Akita <103230912+GitHubBiginner@users.noreply.github.com>
2022-07-05 14:20:19 +0900

    commit

---
+++ Profile/Js/main.js
@@ -0,0 +1,12 @@
+const ham = $('#js-hamburger');
+const nav = $('#js-nav');
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+  ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+  nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外し
+});
+
+const navItem = $('#js-nav a');
+navItem.on('click', function () { 
+    nav.toggleClass('active');
+    ham.toggleClass('active');
+});
